5fc183cf321987621b0a6a2f270066f710f88acf
Supreet Praharaj <supreet.praharaj.pro+github@gmail.com>
2023-06-09 13:36:41 +0530

    Check token

--- app/model/model.py
+++ app/model/model.py
@@ -10,6 +10,10 @@ from config import settings
 
 __version__ = "0.1.0"
 
+print('-----------------------------------------------------------')
+print(os.environ.get('HF_TOKEN'))
+print('-----------------------------------------------------------')
+
 # BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 
